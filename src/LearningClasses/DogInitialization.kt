82c5d5d9102ff3519d498fe98f1fe38ffa209420
Laura Roa <laualexandrar@gmail.com>
2020-06-15 19:18:49 -0700

    Learning initialization

---
+++ src/LearningClasses/DogInitialization.kt
@@ -0,0 +1,28 @@
+package LearningClasses
+
+class DogInitialization (val name: String, var weight: Int, breed_param: String ){
+    init {
+        println("Dog $name has been created")
+    }
+
+    var activities = arrayOf("Walk")
+    val breed = breed_param.toUpperCase()
+    var temperament: String = "mad "
+
+    init {
+        println("The breed is $breed")
+    }
+
+    override fun toString(): String {
+        return "When he goes for a ${activities[0]} , he is always $temperament "
+    }
+
+}
+
+fun main() {
+
+    val myDog = DogInitialization ("Fido", 67, "Samoyedo")
+    println(myDog.toString())
+
+
+}
